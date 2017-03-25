ba0595534fa8fb459240bb70f528b2a26b9b0b5b
Hedron_JJenkins <jessetjenkins@gmail.com>
2017-03-25 16:22:45 -0700

    added WTF folder

---
+++ WTF/untitled1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 25 15:00:38 2017
+
+@author: root
+"""
+
+import ST_VL6180X
+
+getDistance = VL6180X.get_distance()
+
+print(getDistance)
